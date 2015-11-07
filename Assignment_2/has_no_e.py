c2efbde236089626e9abb93235891c34cb09fe42
Andrei Stoica <andrei.stoica@uoit.net>
2015-11-07 15:50:03 -0500

    removed debug code

--- Assignment_2/has_no_e.py
+++ Assignment_2/has_no_e.py
@@ -4,7 +4,6 @@ def has_no_e(string):
 reader = open('gadsby_stripped.txt', 'r')
 line = reader.readline()
 while line != '' :
-    print line
     print (has_no_e(line))
     line = reader.readline()
 reader.close()
