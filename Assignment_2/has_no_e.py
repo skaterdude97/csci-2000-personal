cc3b3b8474de292e94e875e8bdeb34306d4ea786
Andrei Stoica <andrei.stoica@uoit.net>
2015-11-07 15:40:46 -0500

    Completed the Assignment

---
+++ Assignment_2/has_no_e.py
@@ -0,0 +1,10 @@
+def has_no_e(string):
+    return not 'e' in string
+
+reader = open('gadsby_stripped.txt', 'r')
+line = reader.readline()
+while line != '' :
+    print line
+    print (has_no_e(line))
+    line = reader.readline()
+reader.close()
